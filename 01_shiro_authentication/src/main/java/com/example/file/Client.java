957b09d9c40da821c6ff42460d73e789e1b87ab8
张奉献 <13669005514@163.com>
2018-12-18 15:26:19 +0800

    zuoye

---
+++ 01_shiro_authentication/src/main/java/com/example/file/Client.java
@@ -0,0 +1,22 @@
+package com.example.file;
+
+import java.io.IOException;
+
+/**
+ * @Description:
+ * 读取指定文件 按格式排放
+ * @Auther: zhangfx
+ * @Date: 2018/12/18/ 11:41
+ */
+public class Client {
+
+    public static void main(String[] args) throws IOException {
+
+        FileUntil.read();
+
+
+    }
+
+
+
+}
